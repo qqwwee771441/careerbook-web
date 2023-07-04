c158428f7aa8d22872a667e453691fd8a90afa21
FiveStarK <gbkos@naver.com>
2023-07-04 15:57:20 +0900

    divide personal comapany

---
+++ signup/signup.js
@@ -0,0 +1,6 @@
+let buttonPersonal = document.querySelector("button:first-child").addEventListener("click", function(){
+    window.location.href = "personal.html"
+})
+let buttonCompany = document.querySelector("button:last-child").addEventListener("click", function() {
+    window.location.href = "company.html"
+})
