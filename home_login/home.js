7c1f08744401f7aa3d3c2adbff4fcb5fb21fb8a3
FiveStarK <gbkos@naver.com>
2023-07-05 01:33:02 +0900

    modify login button

--- home_login/home.js
+++ home_login/home.js
@@ -1,3 +1,18 @@
 let signup = document.querySelector(".signup span").addEventListener("click", function() {
     window.location.href = "/signup/signup.html"
 })
+
+const username = document.querySelector('#username');
+const password = document.querySelector('#password');
+const submitButton = document.querySelector('button[type="submit"]');
+
+function checkInput() {
+    if (username.value && password.value.length >= 8) {
+        submitButton.removeAttribute('disabled');
+    } else {
+        submitButton.setAttribute('disabled', true);
+    }
+}
+
+username.addEventListener('input', checkInput);
+password.addEventListener('input', checkInput);
